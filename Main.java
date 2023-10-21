3a6b68ded8d507870fde94c9075638b1c069b091
cynzhra <cynthiazhra@gmail.com>
2023-10-21 20:22:16 +0700

    tambah main

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Item item = new Item();
+        Order order = new Order();
+        order.addItem(item);
+        OrderHistory history = new OrderHistory();
+        history.getDailyHistory();
+        OrderViewer viewer = new OrderViewer();
+        viewer.printOrder(order);
+    }
+}
