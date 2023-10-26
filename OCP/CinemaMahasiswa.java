3b173a818e70a09f66f11f31bcb0d8c065f5412a
cynzhra <cynthiazhra@gmail.com>
2023-10-26 11:05:05 +0700

    Memasukkan file kedalam folder

---
+++ OCP/CinemaMahasiswa.java
@@ -0,0 +1,10 @@
+class CinemaMahasiswa extends Cinema {
+    public CinemaMahasiswa(double price) {
+        this.price = price;
+    }
+
+    @Override
+    Double calculateAdminFee() {
+        return price * 5 / 100;
+    }
+}
